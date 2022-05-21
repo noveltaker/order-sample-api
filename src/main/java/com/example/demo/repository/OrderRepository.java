78d92f5b458e62beef6dcf321947a47ab8bf30c5
gon-zo <pnw123456@icloud.com>
2022-05-21 19:54:27 +0900

    test: 유저별 주문 조회 테스트 케이스

--- src/main/java/com/example/demo/repository/OrderRepository.java
+++ src/main/java/com/example/demo/repository/OrderRepository.java
@@ -1,8 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+  Page<Order> findAllByUserId(Pageable pageable, Long userId);
+
+  Page<Order> findAllByProductId(Pageable pageable, Long product);
+}
--- src/test/java/com/example/demo/repository/OrderRepositoryTest.java
+++ src/test/java/com/example/demo/repository/OrderRepositoryTest.java
@@ -2,13 +2,16 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Order;
 import com.example.demo.mock.OrderMock;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
@@ -30,5 +33,40 @@ class OrderRepositoryTest {
     Assertions.assertEquals(mock.getCount(), entity.getCount());
     Assertions.assertEquals(mock.getState(), entity.getState());
     Assertions.assertEquals(mock.getDate(), entity.getDate());
+  }
+
+  @Nested
+  @DisplayName("조회")
+  public class Select {
+
+    private Order mock;
+
+    @BeforeEach
+    void init() {
+      mock = orderRepository.save(OrderMock.createdMock());
+    }
+
+    @Test
+    @DisplayName("유저별 주문 모록 조회")
+    void findAllByUserId() {
+
+      final Long userId = 1L;
+
+      PageRequest pageable = PageRequest.of(0, 10);
+
+      Page<Order> pageList = orderRepository.findAllByUserId(pageable, userId);
+
+      List<Order> content = pageList.getContent();
+
+      Assertions.assertEquals(1, content.size());
+
+      Order entity = content.get(0);
+      org.assertj.core.api.Assertions.assertThat(entity).isEqualTo(mock);
+      Assertions.assertEquals(mock.getId(), entity.getId());
+      Assertions.assertEquals(mock.getUserId(), entity.getUserId());
+      Assertions.assertEquals(mock.getCount(), entity.getCount());
+      Assertions.assertEquals(mock.getState(), entity.getState());
+      Assertions.assertEquals(mock.getDate(), entity.getDate());
+    }
   }
 }
