ba6a3e2d45ea597c70087f51c4506972e2d53295
noveltaker <pnw123456@icloud.com>
2022-06-28 22:52:52 +0900

    chore: remvoe value

--- src/main/java/com/example/demo/contracts/SecurityConstants.java
+++ src/main/java/com/example/demo/contracts/SecurityConstants.java
@@ -2,6 +2,4 @@ package com.example.demo.contracts;
 
 public class SecurityConstants {
   public static final String TOKEN_PREFIX = "Bearer ";
-
-  public static final String ROLE_USER = "ROLE_USER";
 }
