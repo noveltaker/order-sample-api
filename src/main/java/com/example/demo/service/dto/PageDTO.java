16d2a64a03906bbbd02bb14bed7b46aeae6aacab
noveltaker <pnw123456@icloud.com>
2022-06-30 12:08:27 +0900

    chore: getter 추가

--- src/main/java/com/example/demo/service/dto/PageDTO.java
+++ src/main/java/com/example/demo/service/dto/PageDTO.java
@@ -1,10 +1,12 @@
 package com.example.demo.service.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.data.domain.PageRequest;
 
 import javax.validation.constraints.NotNull;
 
+@Getter
 @AllArgsConstructor
 public class PageDTO {
 
